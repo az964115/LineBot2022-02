5a27a5ee709ef92c2bd59182dda4e078d93f53f2
az964115 <98249687+az964115@users.noreply.github.com>
2022-02-10 21:24:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ def index():
                                         "uri": f"tel:{my_phone}"
                                       }
                                   ]
-                              }
+                                }
                             }
                         ]
                 else:
@@ -172,7 +172,7 @@ def sendTextMessageToMe():
 def getNameEmojiMessage():
     lookUpStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     productId = "5ac21a8c040ab15980c9b43f"
-    name = "Jason"
+    name = "Miles"
     message = dict()
     message["type"] = "text"
     message["text"] = "".join("$" for r in range(len(name)))
