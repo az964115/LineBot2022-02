e0f22effdaa13e9a020b7b104f34be78ff1b463e
az964115 <98249687+az964115@users.noreply.github.com>
2022-02-18 16:19:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -92,22 +92,22 @@ def index():
                                         "type": "message",
                                         "label": "TC-100%無糖巧克力磚",
                                         "text": "TC-100%無糖巧克力磚"
-                                      },
-                                      {
-                                        "type": "message",
-                                        "label": "TC-95%巧克力磚",
-                                        "text": "TC-95%巧克力磚"
-                                      },
+                                      } #,
+#                                       {
+#                                         "type": "message",
+#                                         "label": "TC-95%巧克力磚",
+#                                         "text": "TC-95%巧克力磚"
+#                                       },
 #                                       {
 #                                         "type": "message",
 #                                         "label": "TC-85%巧克力磚",
 #                                         "text": "TC-85%巧克力磚"
 #                                       },
-                                      {
-                                        "type": "uri",
-                                        "label": "聯絡我",
-                                        "uri": f"tel:{my_phone}"
-                                      }
+#                                       {
+#                                         "type": "uri",
+#                                         "label": "聯絡我",
+#                                         "uri": f"tel:{my_phone}"
+#                                       }
                                   ]
                               }
                             }
