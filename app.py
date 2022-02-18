37e5989d158e472bd85d9169e216d23548384b4e
az964115 <98249687+az964115@users.noreply.github.com>
2022-02-18 15:45:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def index():
                     payload["messages"] = [{"type": "text",
                                 "text": "開發中請稍後"}]
                 elif text == "TC-綜合巧克力禮盒":
-                    payload["messages"] = [get1(),
+                    payload["messages"] = [getImageMessage(originalContentUrl),  #get1()
                                           {"type": "text",
                                 "text": "NT$ 550"}]
                 elif text == "TC-柴燒黑糖巧克力":
